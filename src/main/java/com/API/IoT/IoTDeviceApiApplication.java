e0ab943227048b14e243cf5d06562911f6fb44e1
Clézio <cleziojr9@gmail.com>
2024-12-12 09:50:06 -0300

    :tada: Commit inicial

---
+++ src/main/java/com/API/IoT/IoTDeviceApiApplication.java
@@ -0,0 +1,13 @@
+package com.API.IoT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTDeviceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoTDeviceApiApplication.class, args);
+	}
+
+}
