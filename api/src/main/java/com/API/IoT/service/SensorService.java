ef379ab32e71d9510dd61d25c42e04e77ed8eb3e
Clézio <cleziojr9@gmail.com>
2024-12-15 12:30:09 -0300

    :sparkles: feat - Criar sensor_service

---
+++ api/src/main/java/com/API/IoT/service/SensorService.java
@@ -0,0 +1,36 @@
+package com.API.IoT.service;
+
+import com.API.IoT.dto.mapper.UserMapper;
+import com.API.IoT.dto.user.SensorCreateDTO;
+import com.API.IoT.dto.user.SensorResponseDTO;
+import com.API.IoT.entity.SensorEntity;
+import com.API.IoT.exception.EntityNotFoundException;
+import com.API.IoT.repository.SensorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class SensorService {
+    @Autowired
+    private SensorRepository sensorRepository;
+
+    @Transactional
+    public void save(SensorCreateDTO sensorCreateDTO) {
+        sensorRepository.save(sensorMapper.toSensor(sensorCreateDTO));
+    }
+
+    @Transactional(readOnly = true)
+    public List<SensorEntity> findAll() {
+        return sensorRepository.findAll();
+    }
+
+    @Transactional
+    public SensorEntity findById(Long id) {
+        return sensorRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Sensor não existe")
+        );
+    }
+}
