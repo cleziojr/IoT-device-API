2cf9f30d4ea2f38845e7e78655dd4c14cd34430b
Clézio <cleziojr9@gmail.com>
2024-12-15 14:03:08 -0300

    :sparkles: feat - (sensor) adiciona Controller e efetua ajustes no Service

--- api/src/main/java/com/API/IoT/service/SensorService.java
+++ api/src/main/java/com/API/IoT/service/SensorService.java
@@ -34,4 +34,46 @@ public class SensorService {
                 () -> new EntityNotFoundException("Sensor não existe")
         );
     }
+
+    @Transactional
+    public void delete(Long id) {
+        sensorRepository.deleteById(id);
+    }
+
+    @Transactional
+    public long count() {
+        return sensorRepository.count();
+    }
+
+    @Transactional
+    public List<SensorEntity> findByFilter(Float ph, Integer waterTemperature, Integer ambientTemperature, Integer humidity, Float floatLevel) {
+        return sensorRepository.findByFilter(ph, waterTemperature, ambientTemperature, humidity, floatLevel);
+    }
+
+    @Transactional
+    public SensorEntity partialUpdate(Long id, SensorCreateDTO sensorCreateDTO) {
+        SensorEntity sensor = sensorRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Sensor não encontrado"));
+
+        if (sensorCreateDTO.getPh() != null) {
+            sensor.setPh(sensorCreateDTO.getPh());
+        }
+        if (sensorCreateDTO.getWaterTemperature() != null) {
+            sensor.setWaterTemperature(sensorCreateDTO.getWaterTemperature());
+        }
+        if (sensorCreateDTO.getAmbientTemperature() != null) {
+            sensor.setAmbientTemperature(sensorCreateDTO.getAmbientTemperature());
+        }
+        if (sensorCreateDTO.getHumidity() != null) {
+            sensor.setHumidity(sensorCreateDTO.getHumidity());
+        }
+        if (sensorCreateDTO.getFloatLevel() != null) {
+            sensor.setFloatLevel(sensorCreateDTO.getFloatLevel());
+        }
+
+        sensor.setUpdatedAt(java.sql.Timestamp.valueOf(java.time.LocalDateTime.now()));
+
+        return sensorRepository.save(sensor);
+    }
+    
 }
