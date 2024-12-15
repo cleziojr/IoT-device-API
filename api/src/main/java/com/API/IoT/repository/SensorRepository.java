74284928bbb1733c2e8ca8fc6a0873b1b02b4cf4
Clézio <cleziojr9@gmail.com>
2024-12-15 13:21:45 -0300

    :truck: chore - renomeia path de importação

--- api/src/main/java/com/API/IoT/repository/SensorRepository.java
+++ api/src/main/java/com/API/IoT/repository/SensorRepository.java
@@ -1,6 +1,6 @@
 package com.API.IoT.repository;
 
-import com.API.IoT.dto.user.SensorResponseDTO;
+import com.API.IoT.dto.sensor.SensorResponseDTO;
 import com.API.IoT.entity.SensorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
