93d6d36862e6d74cfd611f148b6d24a39a3dd969
Clézio <cleziojr9@gmail.com>
2024-12-15 13:23:38 -0300

    :label: chore - substitui classe por interface

--- api/src/main/java/com/API/IoT/repository/SensorRepository.java
+++ api/src/main/java/com/API/IoT/repository/SensorRepository.java
@@ -5,7 +5,5 @@ import com.API.IoT.entity.SensorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public class SensorRepository extends JpaRepository<SensorEntity, Long>{}
+public interface SensorRepository extends JpaRepository<SensorEntity, Long>{}
